16ab52ea849ef9851c0eb22f088f534b3b1c8593
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-09 18:35:13 +0000

    Added review summary by requirement

--- client/views/summary.js
+++ client/views/summary.js
@@ -7,6 +7,22 @@ function getReviewResult (review) {
   return r;
 }
 
+function getRequirementReview (rev, req) {
+  // Find in review result for particular requiremnt
+  if (!rev || !req) return;
+
+  var res = {
+    user: rev.user,
+    requirement: req
+  };
+
+  var r = lodash.find(rev.results, ['_id', req]);
+  res.result = r && r.result ? r.result.value || 0 : 0;
+  res.feedback = r && r.result ? r.result.comment : undefined;
+
+  return res;
+}
+
 function getAssignmentScore(assignment) {
   var reqs = Requirements.find({assignment: assignment}).fetch();
   return lodash.sumBy(reqs, 'score');
@@ -25,7 +41,6 @@ Template.work_summary_content.helpers({
   'scoreRate': function(review) {
     var s = getReviewResult(review).earned;
     var t = getAssignmentScore(review.assignment);
-    console.log(t);
     var r = (s || 0) / (t || 1);
     return r >= 0.8 ? 'high' : (r < 0.5 ? 'low' : '');
   },
@@ -34,5 +49,23 @@ Template.work_summary_content.helpers({
   },
   'reviewFeedbackStyle': function (review) {
     return review && review.feedback && review.feedback.trim().length > 0 ? 'full': 'short';
+  },
+  'requirementScoreRate': function (r) {
+    return r && (r.result > 0) ? 'high' : 'low';
+  },
+  'requirementsSummary': function () {
+    var reqs = Requirements.find({assignment: this.assignment}).fetch();
+    var revs = Reviews.find({work: this._id}).fetch();
+    return lodash.map(reqs, function (rq) {
+      rq.reviews = lodash.map(revs, function(rv) {
+        return getRequirementReview(rv, rq._id);
+      });
+
+      var earnd = lodash.filter(rq.reviews, 'result').length;
+      var tot = rq.reviews.length;
+      rq.result = earnd > 0.5 * tot ? rq.score : 0;
+
+      return rq;
+    });
   }
 })
