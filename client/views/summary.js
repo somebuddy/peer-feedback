b18482a381a1e7cfcda71ff4080f5e9c97041e3a
Somebuddy <aleksey.drogovozov@gmail.com>
2016-02-09 04:10:18 +0000

    Added work review summary scores

--- client/views/summary.js
+++ client/views/summary.js
@@ -4,23 +4,35 @@ function getReviewResult (review) {
     t.earned += r.result ? r.result.value || 0 : 0;
     return t;
   }, { earned: 0, total: 0});
-  return r.earned;
+  return r;
+}
+
+function getAssignmentScore(assignment) {
+  var reqs = Requirements.find({assignment: assignment}).fetch();
+  return lodash.sumBy(reqs, 'score');
 }
 
 Template.work_summary_content.helpers({
   'summary': function () {
-    console.log(this);
     var reviews = Reviews.find({work: this._id}).fetch();
   },
   'reviews': function () {
-    console.log(Reviews.find({work: this._id}).count());
     return Reviews.find({work: this._id});
   },
   'reviewScore': function (review) {
-    return getReviewResult(review);
+    return getReviewResult(review).earned;
+  },
+  'scoreRate': function(review) {
+    var s = getReviewResult(review).earned;
+    var t = getAssignmentScore(review.assignment);
+    console.log(t);
+    var r = (s || 0) / (t || 1);
+    return r >= 0.8 ? 'high' : (r < 0.5 ? 'low' : '');
   },
   'requirements': function () {
-    console.log(this);
     return Requirements.find({assignment: this.assignment});
+  },
+  'reviewFeedbackStyle': function (review) {
+    return review && review.feedback && review.feedback.trim().length > 0 ? 'full': 'short';
   }
 })
