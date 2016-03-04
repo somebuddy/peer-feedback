c041d45db5df8cf929cebc0c0f65da651a8ce162
Somebuddy <aleksey.drogovozov@gmail.com>
2016-03-04 13:03:17 +0000

    Cleaned up assignment state styles

--- client/components/assignment-state.js
+++ client/components/assignment-state.js
@@ -1,27 +1,40 @@
 /*global Works, Router*/
 
+function getUserWorks (assignment) {
+  return Works.find({
+    assignment: assignment,
+    user: Meteor.userId(),
+  }, {sort: {createdAt: -1}});
+}
+
 Template.assignment_state.helpers({
-  'canReview': function () {
-    return Works.find({assignment:this._id}).count() > 0;
-  },
-  'submittionCount': function () {
-    return Works.find({assignment:this._id}).count();
-  },
   'lastWork': function () {
     Meteor.subscribe('user-works-for-assignment', this._id);
     return Works.findOne({
       assignment:this._id,
       user: Meteor.userId(),
     }, {sort: {createdAt: -1}});
-  }
+  },
+  'isHistoryExist': function () {
+    Meteor.subscribe('user-works-for-assignment', this._id);
+    console.log(getUserWorks(this._id).count());
+    return getUserWorks(this._id).count() > 1;
+  },
+  'previousWorksCount': function () {
+    Meteor.subscribe('user-works-for-assignment', this._id);
+    return getUserWorks(this._id).count();
+  },
+  'canReview': function () {
+    return Works.find({assignment:this._id}).count() > 0;
+  },
+  'submittionCount': function () {
+    return Works.find({assignment:this._id}).count();
+  },
 });
 
 Template.assignment_state.events({
   'click .submit-button': function (event) {
     $('#submit-work-modal').addClass('show');
-  },
-  'click .history-button': function (event) {
-    $('#previous-works-modal').addClass('show');
   },
   'click .review-button': function (event) {
     Router.go('/make-review/' + this._id);
