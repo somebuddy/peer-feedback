8cae155f0e592651f8c19fe4eb26eec525a0795b
Somebuddy <aleksey.drogovozov@gmail.com>
2016-03-27 18:53:32 +0000

    Clean up report build code

--- client/views/summary.js
+++ client/views/summary.js
@@ -1,48 +1,88 @@
-/*global lodash, Reviews, Requirements, Reports, Works */
+/*global lodash, Reports, Works */
 
 Reports = new Mongo.Collection("reports");
 
-// -----------------------------------------------------------------------------
-// Work summary widget
-// -----------------------------------------------------------------------------
+// Template Helpers
+var getReviewRateClass = (r) => { return r >= 0.8 ? 'high' : (r < 0.5 ? 'low' : '') };
+var getReviewDisplayClass = (r) => { return r && r.feedback && r.feedback.trim().length > 0 ? '': 'short' };
+var getRequirementDisplayClass = (r) => { return r && r.comment && r.comment.trim().length > 0 ? '': 'bubble' };
 
-var getWorkScore = function (report) {
-  // concat all requirements from all reviews
-  var result = lodash(report.result)
-    .values()
-    .reduce(function (t, r) {
-      var results = lodash(r.results)
-        .map((req) => lodash.pick(req, ['title', 'score', 'result', 'task']))
-        .map((req) => lodash.set(req, 'user', r.user))
-        .value();
-      return t.concat(results);
-    }, []);
-
-  // regroup by requirements
-  result = lodash(result)
-    .reduce(function (a, r) {
-      var key = [r.title];
-      if (!a[key]) {
-        a[key] = { reviews: [r]};
-        a[key].title = r.title;
-        a[key].score = r.score;
-      } else {
-        a[key].reviews.push(r);
-      }
-      return a;
-    }, {});
-
-  result = lodash(result)
-    .values()
-    .reduce (function(t, r) {
-      t.result += getRequirementResult(r.reviews);
-      t.total += r.score;
-      return t;
-    }, {result: 0, total: 0});
-
+// Calculators
+var calculateReviewResult = function (review) {
+  var result = lodash.reduce(review.results, function(t, r) {
+    return {
+      total: t.total + r.score || 0,
+      earned: t.earned + (r.result && r.score && r.result.value ? r.result.value * r.score : 0)
+    };
+  }, { earned: 0, total: 0 });
+  result.rate = result.earned / (result.total || 1);
   return result;
 };
 
+var mapReviewScore = (r) => lodash.map(r, (r) => lodash.set(r, 'score', calculateReviewResult(r)));
+
+var calculateReviewsStat = function (report) {
+  if (report && report.result) report.result = mapReviewScore(lodash.values(report.result));
+  return report || {};
+};
+
+var getRequirementsList = function (report) {
+  return lodash(report.result).values()
+    .reduce(function (t, r) {
+      var results = lodash.map(r.results, (req) => lodash.pick(req, ['title', 'score', 'result', 'task']));
+      results = lodash.map(results, (req) => lodash.set(req, 'user', r.user));
+      return t.concat(results);
+    }, []);
+};
+
+var groupRequirements = function(lst, fields) {
+  var reqs = lodash.reduce(lst, function (a, r) {
+      var key = lodash(r).pick(fields).values().value();
+      a[key] = a[key] || {
+        title: r.title,
+        score: r.score,
+        reviews: []
+      };
+      a[key].reviews.push(r);
+      return a;
+    }, {});
+  return lodash.values(reqs);
+};
+
+var getRequirementResult = function (reviews) {
+  var result = lodash(reviews).countBy('result.value').toPairs().maxBy(1)[0];
+  return lodash.toSafeInteger(result);
+};
+
+var calculateRequirementsStat = function(lst) {
+  return lodash.map(lst, function (r) {
+    r.result = getRequirementResult(r.reviews);
+    r.rate = r.result / (r.score || 1);
+    return r;
+  });
+};
+
+var rebuildReportByRequirements = function(report) {
+  if (report && report.result) {
+    var result = getRequirementsList(report);
+    result = groupRequirements(result, ['title', 'score']);
+    report.result = calculateRequirementsStat(result);
+  }
+  return report || {};
+};
+
+var calculateWorkScore = function (report) {
+  report = rebuildReportByRequirements(report);
+  return lodash(report.result).reduce(function(t, r) {
+    t.result += r.result;
+    t.total += r.score;
+    return t;
+  }, {result: 0, total: 0});
+};
+
+// -----------------------------------------------------------------------------
+
+// Work summary
 Template.work_summary_result.onCreated(function () {
   var self = this;
   self.autorun(function () {
@@ -58,49 +98,13 @@ Template.work_summary_result.helpers({
   work: function () {
     return Works.findOne({_id: this.workId});
   },
-  'workScore': function () {
+  workScore: function () {
     var report = Reports.findOne({_id: this.workId});
-    if (report && report.result) {
-      return getWorkScore(report);
-    }
+    return calculateWorkScore(report);
   },
 });
 
-function getAssignmentScore(assignment) {
-  var reqs = Requirements.find({assignment: assignment}).fetch();
-  return lodash.sumBy(reqs, 'score');
-}
-
-// Counters
-
-// -----------------------------------------------------------------------------
 // Review summary
-// -----------------------------------------------------------------------------
-
-// Score for user review
-var reviewResult = function (review) {
-  var result = lodash.reduce(review.results, function(t, r) {
-    return {
-      total: t.total + r.score || 0,
-      earned: t.earned + (r.result && r.score && r.result.value ? r.result.value * r.score : 0)
-    };
-  }, { earned: 0, total: 0 });
-  result.rate = result.earned / (result.total || 1);
-  return result;
-};
-
-var mapReviewScore = (r) => lodash(r).values().map((r) => lodash.set(r, 'score', reviewResult(r))).value();
-
-// Score for particular requirement by all users
-var getRequirementResult = function (reviews) {
-  // count result by simple majority
-  var result = lodash(reviews)
-    .countBy('result.value')
-    .toPairs()
-    .maxBy(1)[0];
-  return lodash.toSafeInteger(result);
-};
-
 Template.work_summary_reviews.onCreated(function () {
   var self = this;
   self.autorun(function () {
@@ -109,23 +113,16 @@ Template.work_summary_reviews.onCreated(function () {
   });
 });
 
-var getReviewRateClass = (r) => { return r >= 0.8 ? 'high' : (r < 0.5 ? 'low' : '') };
-var getReviewDisplayClass = (r) => { return r && r.feedback && r.feedback.trim().length > 0 ? '': 'short' };
-
 Template.work_summary_reviews.helpers({
   reports: function () {
-    var reps = Reports.findOne({_id: this.workId});
-    if (reps && reps.result) reps.result = mapReviewScore(reps.result);
-    return reps || {};
+    var report = Reports.findOne({_id: this.workId});
+    return calculateReviewsStat(report);
   },
   reviewFeedbackStyle: getReviewDisplayClass,
   scoreRate: getReviewRateClass
 });
 
-// -----------------------------------------------------------------------------
 // Requirement statistic
-// -----------------------------------------------------------------------------
-
 Template.work_summary_requirements.onCreated(function () {
   var self = this;
   self.autorun(function () {
@@ -136,46 +133,9 @@ Template.work_summary_requirements.onCreated(function () {
 
 Template.work_summary_requirements.helpers({
   requirements: function() {
-    var reps = Reports.findOne({_id: this.workId});
-    if (reps && reps.result) {
-      // concat all requirements from all reviews
-      var result = lodash(reps.result)
-        .values()
-        .reduce(function (t, r) {
-          var results = lodash(r.results)
-            .map((req) => lodash.pick(req, ['title', 'score', 'result', 'task']))
-            .map((req) => lodash.set(req, 'user', r.user))
-            .value();
-          return t.concat(results);
-        }, []);
-
-      // group requirements
-      result = lodash(result)
-        .reduce(function (a, r) {
-          var key = [r.title, r.score];
-          if (!a[key]) {
-            a[key] = { reviews: [r]};
-            a[key].title = r.title;
-            a[key].score = r.score;
-          } else {
-            a[key].reviews.push(r);
-          }
-          return a;
-        }, {});
-
-      // build the list
-      reps.result = lodash(result)
-        .values()
-        .map(function (r) {
-          r.result = getRequirementResult(r.reviews);
-          r.rate = r.result / (r.score || 1);
-          return r;
-        })
-        .value();
-    }
-    return reps || {};
+    var report = Reports.findOne({_id: this.workId});
+    return rebuildReportByRequirements(report);
   },
-  requirementFeedbackStyle: (r) => { return r && r.result && r.result.comment ? '' : 'bubble'; },
-  requirementScoreRate: (r) => { return r && r.result && r.result.value ? 'high' : 'low'; },
-  requirementSummaryScoreRate: getReviewRateClass
+  requirementFeedbackStyle: getRequirementDisplayClass,
+  requirementScoreRate: getReviewRateClass,
 });
